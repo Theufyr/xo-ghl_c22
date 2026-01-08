674b92e7375b4ea882388c22fe61cd9ad8ead10d
pierrelisandrumartin <pierrelisandrumartin@gmail.com>
2026-01-08 12:09:19 +0100

    add : navbar css and responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
-export default  function Navbar () {
-    return(
-        <>
-        <NavLink to = "/" > accueil </NavLink>
-        <NavLink to = "/jadopte" > j'adopte </NavLink>
-        </>
-    )
-
+export default function Navbar() {
+  return (
+    <nav>
+        <div>
+            <div>
+          <NavLink to="/"><img src="/src/assets/icons/paws.png" alt="pattes" /><span>Ada<span>opte</span></span></NavLink>
+          </div>
+          <div>
+          <NavLink to="/"><img src="/src/assets/icons/home.png" alt="maison" /> accueil </NavLink>
+          <NavLink to="/jadopte"><img src="/src/assets/icons/bone.png" alt="os" /> j'adopte </NavLink>
+          <NavLink to="/"><img src="/src/assets/icons/fish.png" alt="poisson" /> guide de l'adoption </NavLink>
+          <NavLink to="/"><img src="/src/assets/icons/squirrel.png" alt="ecureuil" /> devenir bénévole</NavLink>
+          </div>
+          <div>
+          <NavLink to="/"><span>faire un don 🫶</span></NavLink>
+          </div>
+        </div>
+    </nav>
+  );
 }
