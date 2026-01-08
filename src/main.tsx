4aae7ccedff75ad96ae0ae6b0babc0d181cf8e5f
Theufyr <theufyr@404az.com>
2026-01-08 14:06:10 +0100

    Pc router (#1)
    * feat : router & pages
    
    * add : CSS for footer, update 3 files, create 6 files & delete 4 files
    
    * add : navbar css and responsive
    
    * chore: CSS & navlinks
    
    ---------
    
    Co-authored-by: pierrelisandrumartin <pierrelisandrumartin@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Adopt from './pages/Adopt'
+import Guide from './pages/Guide'
+import Benevole from './pages/Benevole'
+import Donation from './pages/Donation'
 
+
+const router = createBrowserRouter ([
+  {
+    path : "/" ,
+    element : <Home />
+  },
+   {
+    path : "jadopte" ,
+    element : <Adopt />
+  },
+   {
+    path : "guide" ,
+    element : <Guide />
+  },
+   {
+    path : "benevole" ,
+    element : <Benevole />
+  },
+   {
+    path : "don" ,
+    element : <Donation />
+  }
+]  
+) ;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
