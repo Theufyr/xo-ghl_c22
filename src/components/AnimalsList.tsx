9c815e706534179a3b08e1d893c1dc3bd1c8a12d
Theufyr <theufyr@404az.com>
2026-01-12 12:28:32 +0100

    Feat gi/bd (#6)
    * feat: connected the our database into the front, recovered name and age of each animals. Update 5 files and delete 1 file.
    
    * feat: recovered name / age/ type/ breed /city and description from our database. update AnimalsList.tsx, Search.tsx, src/index.css and Adopt.tsx
    
    * modify img name
    
    * update config server
    
    * feat: added the new imgs, update 2 files and create 3 files
    
    ---------
    
    Co-authored-by: Giovana Farinha <giovanafarinha8@gmail.com>
    Co-authored-by: Laurie <lespinasse.laurie5@gmail.com>

---
+++ src/components/AnimalsList.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { sql } from "../lib/sql";
+export default function AnimalsList() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["animals"],
+    queryFn: async () => {
+      const result = await sql(
+        "SELECT animals.id, animals.name AS animal_name, animals.age, animals.description, animals.image_url AS image_url, breeds.breed, types.type, shelters.name AS shelter_name, cities.city FROM animals JOIN breeds ON animals.breed_id = breeds.id JOIN types ON breeds.type_id = types.id JOIN shelters ON animals.shelter_id = shelters.id JOIN cities ON shelters.city_id = cities.id"
+      );
+      if (!result.success) throw new Error(result.error);
+      return result.data;
+    },
+  });
+
+  if (isLoading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur : {error.message}</p>;
+
+  return (
+    <div style={{}}>
+      {data.map((animal: any) => (
+        <section key={animal.id}>
+          <img src={`src/assets/img/${animal.image_url}`} />
+          <p> {animal.type}</p>
+          <p style={{ color: "var(--accent-color)" }}> {animal.animal_name} </p>
+          <p>
+            {animal.age} ans • {animal.breed}
+          </p>
+          <p> {animal.city}</p>
+          <p> {animal.description}</p>{" "}
+        </section>
+      ))}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import Adopt from './pages/Adopt'
-import Guide from './pages/Guide'
-import Benevole from './pages/Benevole'
-import Donation from './pages/Donation'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Adopt from "./pages/Adopt";
+import Guide from "./pages/Guide";
+import Benevole from "./pages/Benevole";
+import Donation from "./pages/Donation";
 
-const router = createBrowserRouter ([
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
   {
-    path : "/" ,
-    element : <Home />
+    path: "/",
+    element: <Home />,
   },
-   {
-    path : "jadopte" ,
-    element : <Adopt />
+  {
+    path: "jadopte",
+    element: <Adopt />,
   },
-   {
-    path : "guide" ,
-    element : <Guide />
+  {
+    path: "guide",
+    element: <Guide />,
   },
-   {
-    path : "benevole" ,
-    element : <Benevole />
+  {
+    path: "benevole",
+    element: <Benevole />,
   },
-   {
-    path : "don" ,
-    element : <Donation />
-  }
-]  
-) ;
-createRoot(document.getElementById('root')!).render(
+  {
+    path: "don",
+    element: <Donation />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router = {router}/>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
