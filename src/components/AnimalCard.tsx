dadd9cf8956f1e4f6bae441627139f8a92c2e1d8
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2026-01-13 14:44:58 +0100

    fix: update AnimalCard.tsx for queryKey

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { sql } from "../lib/sql";
 export default function AnimalCard() {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["animals"],
+    queryKey: ["AnimalCard"],
     queryFn: async () => {
       const result = await sql(
         "SELECT animals.id, animals.name AS animal_name, animals.age, animals.description, animals.image_url AS image_url, breeds.breed, types.type, shelters.name AS shelter_name, cities.city FROM animals JOIN breeds ON animals.breed_id = breeds.id JOIN types ON breeds.type_id = types.id JOIN shelters ON animals.shelter_id = shelters.id JOIN cities ON shelters.city_id = cities.id"
