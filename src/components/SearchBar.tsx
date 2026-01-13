8eb357e69dc2d8e3d98bed0b9d88ef6ae729365c
Giovana Farinha <giovanafarinha8@gmail.com>
2026-01-13 14:09:20 +0100

    feat: updated the search bar with the types and city. Added another file .sql to update the animals in french. create 2 files and update 1 file

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,7 @@
 // import { Form } from "react-router-dom";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { sql } from "../lib/sql";
 export default function SearchBar() {
   const [localisation, setLocalisation] = useState("");
   const [animalType, setAnimalType] = useState("");
@@ -7,6 +9,20 @@ export default function SearchBar() {
     event.preventDefault();
     setLocalisation("");
   }
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["animals"],
+    queryFn: async () => {
+      const result = await sql(
+        "SELECT type, NULL AS city, NULL AS zip_code FROM types UNION ALL SELECT NULL AS type, city, zip_code FROM cities"
+      );
+      if (!result.success) throw new Error(result.error);
+      return result.data;
+    },
+  });
+
+  if (isLoading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur : {error.message}</p>;
+
   return (
     <>
       <form onSubmit={submitFrom}>
@@ -15,14 +31,20 @@ export default function SearchBar() {
             <label htmlFor="">Type d'animal</label>
           </div>
           <select onChange={(event) => setAnimalType(event.target.value)}>
-            <option value="chat">chat </option>
-            <option value="chien">chien</option>
+            {data.map((form: any) =>
+              form.type !== null ? (
+                <option value={form.type}> {form.type} </option>
+              ) : (
+                ""
+              )
+            )}
           </select>
         </div>
         <div>
           <div>
             <label htmlFor="">Localisation</label>
           </div>
+
           <input
             value={localisation}
             onChange={(event) => setLocalisation(event.target.value)}
@@ -35,7 +57,6 @@ export default function SearchBar() {
           <button type="submit">Rechercher 🔎</button>
         </div>
       </form>
-  
     </>
   );
 }
