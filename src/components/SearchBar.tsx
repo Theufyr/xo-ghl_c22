8ab2d6cec3eab2620d26b2327abf9a2e8f68e3e3
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2026-01-14 15:26:07 +0100

    add: on SearchBar : useSearchParams & useNavigate / update SearchBar.tsx and Adopt.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import UseQuerySearchBar from "../hooks/UseQuerySearchBar"
+import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router";
+
 export default function SearchBar() {
-  const [localisation, setLocalisation] = useState("");
-  const [animalType, setAnimalType] = useState("");
-  function submitFrom(event: any) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const getAnimalType = (searchParams.get("animalType")) ?? "";
+  const getLocalisation = (searchParams.get("localisation")) ?? "";
+
+  const [localisation, setLocalisation] = useState(getLocalisation);
+  const [animalType, setAnimalType] = useState(getAnimalType);
+  let navigate = useNavigate();
+  
+  function submitForm(event: any) {
     event.preventDefault();
-    setLocalisation("");
+    const params = new URLSearchParams();
+    params.set("animalType", animalType);
+    params.set("localisation", localisation);
+    const urlSearch = "/jadopte?" + params.toString();
+    navigate(urlSearch);
   }
   const { data, isLoading, error } = UseQuerySearchBar ();
   
@@ -15,12 +28,13 @@ export default function SearchBar() {
   console.log (data);
   return (
     <>
-      <form onSubmit={submitFrom}>
+      <form onSubmit={submitForm}>
         <div>
           <div>
             <label htmlFor="">Type d'animal</label>
           </div>
           <select onChange={(event) => setAnimalType(event.target.value)}>
+            <option value="all">Tous les animaux</option>
             {data.map((form: any) =>
               form.type !== null ? (
                 <option value={form.type}> {form.type} </option>
--- src/pages/Adopt.tsx
+++ src/pages/Adopt.tsx
@@ -1,14 +1,19 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import SearchBar from "../components/SearchBar"
-import AnimalCard from "../components/AnimalCard"
+import AnimalList from "../components/AnimalList"
+import { useSearchParams } from "react-router";
 
 export default function Adopt() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const getAnimalType = searchParams.get("animalType");
+    const getLocalisation = searchParams.get("localisation");
+    console.log(getAnimalType, getLocalisation);
     return (
         <main id="adopt">
             <Navbar />
             <SearchBar />
-            <AnimalCard />
+            <AnimalList />
             <Footer />
         </main>
     )
