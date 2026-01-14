04b2429dc8e0f4749caea1aca4f56cafb7c558cc
pierrelisandrumartin <pierrelisandrumartin@gmail.com>
2026-01-14 11:56:20 +0100

    P hooks (#17)
    * feat:create UseQuerySearchBar
    
    * feat: create typescript interface and animalList

---
+++ src/components/AnimalList.tsx
@@ -0,0 +1,20 @@
+import UseQueryAnimalCard from "../hooks/UseQueryAnimalCard";
+import AnimalCard from "./AnimalCard";
+import type { typesAnimals,typesAnimalInfos } from "../interface/Props";
+
+export default function AnimalList() {
+  const { data, isLoading, error } = UseQueryAnimalCard ();
+  
+
+  if (isLoading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur : {error.message}</p>;
+  
+    const animalDatas: typesAnimals[] = data;
+  return (
+    <div id="animal_card">
+    {animalDatas.map((animal) => (
+        <AnimalCard key={animal.id} animalInfos={animal}/>
+    ))}
+    </div>
+  );
+}
