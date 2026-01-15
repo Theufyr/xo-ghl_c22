8e6896be747bcbb05b698a50eb58cedcd0b7e9a7
Giovana Farinha <giovanafarinha8@gmail.com>
2026-01-15 14:23:47 +0100

    feat: added the props  /update AnimalList.tsx, UseQueryAnimalCard.tsx, Props.ts and Adopt.tsx

--- src/components/AnimalList.tsx
+++ src/components/AnimalList.tsx
@@ -1,20 +1,33 @@
 import UseQueryAnimalCard from "../hooks/UseQueryAnimalCard";
 import AnimalCard from "./AnimalCard";
-import type { typesAnimals } from "../interface/Props";
+import type {
+  typesAnimals,
+  typesParamsAnimalType,
+  typesParamsLocalisation,
+
+} from "../interface/Props";
+import { useSearchParams} from "react-router-dom";
 
 export default function AnimalList() {
-  const { data, isLoading, error } = UseQueryAnimalCard ();
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+  const getAnimalType: typesParamsAnimalType = searchParams.get("animalType");
+  const getLocalisation: typesParamsLocalisation =
+    searchParams.get("localisation");
+
+  const { data, isLoading, error } = UseQueryAnimalCard(
+    getAnimalType,
+    getLocalisation
+  );
 
   if (isLoading) return <p>Chargement...</p>;
   if (error) return <p>Erreur : {error.message}</p>;
-  
-    const animalDatas: typesAnimals[] = data;
+
+  const animalDatas: typesAnimals[] = data;
   return (
     <div id="animal_card">
-    {animalDatas.map((animal) => (
-        <AnimalCard key={animal.id} animalInfos={animal}/>
-    ))}
+      {animalDatas.map((animal) => (
+        <AnimalCard key={animal.id} animalInfos={animal} />
+      ))}
     </div>
   );
 }
