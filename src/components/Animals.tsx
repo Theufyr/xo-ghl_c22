8d72519834ab689f4ffda2ae3fbadff2fd1cfc96
Theufyr <theufyr@404az.com>
2026-01-17 11:04:57 +0100

    add: link to top Adopt page on Home page all pets button / update Animals.tsx and Home.tsx (#28)

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,11 +1,18 @@
-import AnimalGridList from "./AnimalGridList"
+import AnimalGridList from "./AnimalGridList";
+import { useNavigate } from "react-router";
+
 export default function Animals() {
-return(
-    <section id="animals">
-        <h2>nos animaux à adopter</h2>
-        <p>Découvrez en images tous nos animaux qui attendent une famille aimante. Chaque photo raconte une histoire et un espoir.</p>
-        <AnimalGridList />
-        <button className="button1">Voir tous les animaux</button>
-    </section>
-)
+    let navigate = useNavigate();
+
+    return(
+        <section id="animals">
+            <h2>nos animaux à adopter</h2>
+            <p>Découvrez en images tous nos animaux qui attendent une famille aimante. Chaque photo raconte une histoire et un espoir.</p>
+            <AnimalGridList />
+            <button className="button1" onClick={() => {
+                    navigate("/jadopte"),
+                    window.scrollTo({top: 0, left: 0, behavior: "smooth"})
+                }}>Voir tous les animaux</button>
+        </section>
+    )
 }
