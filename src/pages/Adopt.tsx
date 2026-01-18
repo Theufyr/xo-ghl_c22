265c34e812c491414f859a1fa0aa31c91db97b4d
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2026-01-18 20:50:28 +0100

    add: paging component / update src/index.css and Adopt.tsx

--- src/pages/Adopt.tsx
+++ src/pages/Adopt.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import SearchBar from "../components/SearchBar"
 import AnimalList from "../components/AnimalList"
+import Paging from "../components/Paging"
 
 export default function Adopt() {
     return (
@@ -9,6 +10,7 @@ export default function Adopt() {
             <Navbar />
             <SearchBar />
             <AnimalList />
+            <Paging />
             <Footer />
         </main>
     )
