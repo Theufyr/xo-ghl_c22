23768d12528f40ed9d04d5981a4b1efd901491ac
Theufyr <theufyr@404az.com>
2026-01-18 15:25:44 +0100

    enhance: pictures grid on Home page / update AnimalGrid.tsx, UseQueryAnimalGrid.tsx and src/index.css (#30)

--- src/components/AnimalGrid.tsx
+++ src/components/AnimalGrid.tsx
@@ -2,6 +2,8 @@ import type { typesAnimalInfos } from "../interface/Props";
 
 export default function AnimalGrid({ animalInfos }: typesAnimalInfos) {
     return(
-        <img src={`./src/assets/img/${animalInfos.image_url}`} alt="photo d'animal" />
+        <div className="grid_card">
+            <img src={`./src/assets/img/${animalInfos.image_url}`} alt="photo d'animal" />
+        </div>
     )
 }
