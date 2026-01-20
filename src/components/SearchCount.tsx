81887bbcde03d5bc4bfbb9433292221c44507a8e
Theufyr <theufyr@404az.com>
2026-01-20 14:20:04 +0100

    T last fix (#35)
    * fix: update adaopte.sql and SearchBar.tsx
    
    * fix: paging buttons / update SearchCount.tsx and src/index.css

--- src/components/SearchCount.tsx
+++ src/components/SearchCount.tsx
@@ -3,12 +3,14 @@ import UseQueryCountResult from "../hooks/UseQueryCountResult";
 import { useNavigate } from "react-router";
 
 export default function SearchCount({setAnimalType, setLocalisation}: any) {
+    // réinitialisation des filtres
     let navigate = useNavigate();
     function reinitSearchParams() {
         setAnimalType("all");
         setLocalisation("");
         navigate("/jadopte")
     }
+    // récupération du nombre de résultats
     const queryCount = UseQueryCountResult();
     if (queryCount.isLoading) return <p>Chargement...</p>;
     if (queryCount.error) return <p>Erreur : {queryCount.error.message}</p>;
